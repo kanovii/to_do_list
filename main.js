2199834e34fc424145ebd5824de63b50c6c252bc
kanovii <kkh0510ks@daum.net>
2022-01-25 17:16:30 +0900

    prepend

--- main.js
+++ main.js
@@ -2,30 +2,55 @@ let input = document.getElementById("input");
 let button = document.getElementById("button");
 let list = document.getElementById("list");
 
-// function addList() {
-//     var temp = document.createElement("li");
-//     temp.innerHTML = input.value;
-//     list.appendChild(temp);    
-// }
-
-const addList = (e) => {
-    console.log(this)
-
-    var temp = document.createElement("li");
-    temp.innerHTML = input.value;
-    list.appendChild(temp);
-    input.value = "";
-}
-
 const enter = (e) => {
-    e.preventDefault();
-    if(e.keyCode === 13) {
+    if(e.keyCode ===13) {
+        e.preventDefault();
         var temp = document.createElement("li");
-        temp.innerHTML = input.value
-        list.appendChild(temp);
+        temp.innerHTML = input.value;
+        list.prepend(temp);
         input.value = "";
     }
 }
 
-input.addEventListener("keypress", enter)
-button.addEventListener("click", addList);
+const clickB = () => {
+    var temp = document.createElement("li");
+    temp.innerHTML = input.value;
+    list.prepend(temp);
+    input.value = "";
+}
+
+input.addEventListener("keypress", enter);
+button.addEventListener("click", clickB);
+
+// recap
+// let input = document.getElementById("input");
+// let button = document.getElementById("button");
+// let list = document.getElementById("list");
+
+// // function addList() {
+// //     var temp = document.createElement("li");
+// //     temp.innerHTML = input.value;
+// //     list.appendChild(temp);    
+// // }
+
+// const addList = (e) => {
+//     console.log(this)
+
+//     var temp = document.createElement("li");
+//     temp.innerHTML = input.value;
+//     list.appendChild(temp);
+//     input.value = "";
+// }
+
+// const enter = (e) => {
+//     e.preventDefault();
+//     if(e.keyCode === 13) {
+//         var temp = document.createElement("li");
+//         temp.innerHTML = input.value
+//         list.appendChild(temp);
+//         input.value = "";
+//     }
+// }
+
+// input.addEventListener("keypress", enter)
+// button.addEventListener("click", addList);
